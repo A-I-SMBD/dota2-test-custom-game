838bd3be416459ad2ab2f896567fc60f4ae5acde
A-I-SMBD <soltan.mamedov22@gmail.com>
2024-10-05 00:00:09 +0700

    fix: добавлен process.exit() для функции запуска доты

--- scripts/launch.js
+++ scripts/launch.js
@@ -10,7 +10,8 @@ const { getAddonName, getDotaPath } = require("./utils");
     // For example `+dota_launch_custom_game ${getAddonName()} dota` would automatically load "dota" map
     const args = ["-novid", "-tools", "-addon", getAddonName()];
     spawn(path.join(win64, "dota2.exe"), args, { detached: true, cwd: win64 });
-})().catch(error => {
+    process.exit(0);
+})().catch((error) => {
     console.error(error);
     process.exit(1);
 });
